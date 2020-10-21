986a3584b376ff9333381a57f81fe1149d40a3f9
unknown <lklorenzen@gmail.com>
2020-10-21 21:02:08 +0200

    Crwn-e-commerce app second deployment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51Hcf80ADYN8VN2B0T7zB0M3SJzY5IOWbEeAig7YgbuHvHKI2AHPZDMydskYAv0TcGeMh5m5LoaXHMKZgGsy84GbD008AJZQoGO';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
